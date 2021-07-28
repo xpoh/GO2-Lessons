39cc9acea3666a0cd728f256b49b6e34504bca54
Dmitriy Rekunov <laddr@mail.ru>
2021-07-28 21:30:11 +0300

    Lesson 4

---
+++ Lesson 4/less4.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type data struct {
+	counter int
+}
+
+func (d *data) increment(ctx context.Context, ch chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case v := <-ch:
+			if v < 1000 {
+				ch <- v + 1
+			} else {
+				ch <- v
+			}
+		default:
+
+		}
+	}
+}
+
+func main() {
+	chsig := make(chan os.Signal, 1)
+	defer close(chsig)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	ctx, cancel = signal.NotifyContext(ctx, syscall.SIGTERM)
+	defer cancel()
+
+	var ch = make(chan int)
+	defer close(ch)
+
+	d := data{counter: 0}
+	for i := 0; i < 1000; i++ {
+		go d.increment(ctx, ch)
+	}
+	ch <- 1
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("Sleep done")
+	cancel()
+
+	v, _ := <-ch
+	fmt.Println("Total count", v)
+}
