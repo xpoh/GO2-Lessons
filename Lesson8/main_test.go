e64c7334ed09c6b8f3bc67f534779f1574f645c0
Dmitriy Rekunov <laddr@mail.ru>
2021-08-13 22:12:15 +0300

    Lesson 8

--- Lesson8/main_test.go
+++ Lesson8/main_test.go
@@ -1,20 +1,24 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGetFileList(t *testing.T) {
 	list, err := getFileList("./")
 	if err != nil {
 		t.Error("err!=nil")
 	}
-	if len(list) != 2 {
+	if len(list) != 3 {
 		t.Error("len(list)!=2")
 	}
 	if list[0].fs.Name() != "main.go" {
 		t.Error("list[0].fs.Name()!=main.go")
 	}
 	if list[1].fs.Name() != "main_test.go" {
-		t.Error("list[0].fs.Name()!=main_test.go")
+		t.Error("list[1].fs.Name()!=main_test.go")
 	}
-
+	if list[2].fs.Name() != "test1.txt" {
+		t.Error("list[2].fs.Name()!=test.txt")
+	}
 }
